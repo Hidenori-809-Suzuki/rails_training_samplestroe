48f338ece29e63d15c8d010436a183a3afba1c47
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2023-01-16 02:10:08 +0900

    create-orderscontroller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+  def cart
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'orders/cart'
+  get 'orders/new'
+  get 'orders/create'
+  get 'orders/destroy'
+  get 'orders/show'
+  get 'orders/index'
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
